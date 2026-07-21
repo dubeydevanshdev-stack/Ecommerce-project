3bab2742f5e6b2d0f167e0b43aa7dd20217c8454
Devansh <dubey.devansh.dev@gmail.com>
2026-07-21 17:39:56 +0530

    Changing to promises to async await

--- react-project/src/Pages/home/HomePage.jsx
+++ react-project/src/Pages/home/HomePage.jsx
@@ -9,10 +9,14 @@ export function HomePage({cart}){
   const [products,setProducts]=useState([]);
 
   useEffect(()=>{
-    axios.get(`/api/products`)
-    .then((response)=>{
+
+    const getHomeData = async()=>{
+      const response =await axios.get(`/api/products`);
       setProducts(response.data);
-      });
+      };
+
+      getHomeData();
+      
   },[]);
   
     return(
