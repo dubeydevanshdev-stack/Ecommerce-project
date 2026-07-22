57fca2b3f47a663558598b0ecab467716b3d0f39
Devansh <dubey.devansh.dev@gmail.com>
2026-07-22 05:39:24 +0530

    Adding useNavigate to place your  order button

--- react-project/src/Pages/checkout/PaymentSummary.jsx
+++ react-project/src/Pages/checkout/PaymentSummary.jsx
@@ -1,6 +1,17 @@
+import axios from "axios";
 import { formatMoney } from "../../utils/money";
+import {useNavigate} from 'react-router';
 
-export function PaymentSummary( {paymentSummary} ) {
+export function PaymentSummary( {paymentSummary, loadCart} ) {
+
+  const navigate=useNavigate();
+
+  const createOrder= async()=>{
+   await axios.post('/api/orders');
+   await loadCart();
+   navigate('/orders');
+  };
+
     return (
         <div className="payment-summary">
                     <div className="payment-summary-title">
@@ -45,7 +56,8 @@ export function PaymentSummary( {paymentSummary} ) {
                         </div>
                     </div>
         
-                    <button className="place-order-button button-primary">
+                    <button className="place-order-button button-primary"
+                    onClick={createOrder}>
                       Place your order
                     </button>
         
