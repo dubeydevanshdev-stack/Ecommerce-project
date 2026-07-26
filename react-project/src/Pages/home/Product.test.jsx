35dd3a3f8a126803b0b07a21ccde396ee21d0e6a
Devansh <dubey.devansh.dev@gmail.com>
2026-07-26 08:48:05 +0530

    Using beforeEach to recreate the test data

--- react-project/src/Pages/home/Product.test.jsx
+++ react-project/src/Pages/home/Product.test.jsx
@@ -1,80 +1,67 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Products } from "./Products.jsx";
-import userEvent from '@testing-library/user-event';
+import userEvent from "@testing-library/user-event";
 import axios from "axios";
 
-vi.mock('axios');
+vi.mock("axios");
 
 describe("Product component", () => {
-  it("displays the product details", () => {
+  let product;
+  let loadCart;
 
-    const product = {
+  beforeEach(()=>{
+
+    product= {
       id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-      image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-      name: "Adults Plain Cotton T-Shirt - 2 Pack",
-      rating: {
-        stars: 4.5,
-        count: 56,
-      },
-      priceCents: 799,
-      keywords: ["tshirts", "apparel", "mens"],
-    };
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56,
+    },
+    priceCents: 799,
+    keywords: ["tshirts", "apparel", "mens"],
+  };
 
-    const loadCart = vi.fn();
+   loadCart = vi.fn();
+    });
 
+  it("displays the product details", () => {
     render(<Products product={product} loadCart={loadCart} />);
 
     expect(
-      screen.getByText("Adults Plain Cotton T-Shirt - 2 Pack")
+      screen.getByText("Adults Plain Cotton T-Shirt - 2 Pack"),
     ).toBeInTheDocument();
 
-    expect(
-        screen.getByText('$7.99')).toBeInTheDocument();
+    expect(screen.getByText("$7.99")).toBeInTheDocument();
 
-    expect(
-        screen.getByTestId('product-image')
-      ).toHaveAttribute('src','images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg');
+    expect(screen.getByTestId("product-image")).toHaveAttribute(
+      "src",
+      "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    );
 
-    expect(
-      screen.getByTestId('product-rating-stars-image')
-    ).toHaveAttribute('src' , 'images/ratings/rating-45.png');
+    expect(screen.getByTestId("product-rating-stars-image")).toHaveAttribute(
+      "src",
+      "images/ratings/rating-45.png",
+    );
 
-    expect(
-      screen.getByText('56')
-    ).toBeInTheDocument();
-
+    expect(screen.getByText("56")).toBeInTheDocument();
   });
 
-  it('Adds a product to the cart', async () =>{
-    const product = {
-      id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-      image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-      name: "Adults Plain Cotton T-Shirt - 2 Pack",
-      rating: {
-        stars: 4.5,
-        count: 56,
-      },
-      priceCents: 799,
-      keywords: ["tshirts", "apparel", "mens"],
-    };
-
-    const loadCart = vi.fn();
-
-
+  it("Adds a product to the cart", async () => {
     render(<Products product={product} loadCart={loadCart} />);
 
-    const user= userEvent.setup();
-    const addToCart =screen.getByTestId('add-to-cart-button');
+    const user = userEvent.setup();
+    const addToCart = screen.getByTestId("add-to-cart-button");
     await user.click(addToCart);
 
-    expect(axios.post).toHaveBeenCalledWith(
-      '/api/cart-items',{
-        productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-        quantity: 1
-      });
+    expect(axios.post).toHaveBeenCalledWith("/api/cart-items", {
+      productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+      quantity: 1,
+    });
 
-      expect(loadCart).toHaveBeenCalled();
+    expect(loadCart).toHaveBeenCalled();
   });
 });
